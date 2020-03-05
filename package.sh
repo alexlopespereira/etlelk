a1910796e175f37fb13e14ba6e1a0f148d1096d3
alex <alex.pereira@planejamento.gov.br>
2020-03-05 11:31:38 -0300

    renamed variable KIBANA_SAVED_OBJECTS_PATH

--- package.sh
+++ package.sh
@@ -1,3 +1,12 @@
-#Edit the version and the download_url from setup.py prior to executing this
+#!/bin/bash
+#Edit the version and the download_url from setup_template.py prior to executing this
+tag=$1
+message=$2
+
+sed 's/TAG/$tag/g' ./setup_template.py > ./setup.py
+
+git commit . -m "$message"
+git tag $tag
+git push origin $tag
 python setup.py sdist
 twine upload dist/*
