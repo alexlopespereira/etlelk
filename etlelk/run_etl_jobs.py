d21538697dc9392f36ead6423294be7c24db79a0
alex <alex.pereira@planejamento.gov.br>
2020-03-05 11:08:10 -0300

    test

--- etlelk/run_etl_jobs.py
+++ etlelk/run_etl_jobs.py
@@ -1,5 +1,6 @@
 from importlib import import_module
 
+
 def run_etl_job(config, job_description):
     module = import_module(job_description['module_name'])
     Cls = getattr(module, job_description['class_name'])
