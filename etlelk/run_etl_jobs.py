aaccc75ba0783516fba613dc6205db0948401a74
Ronnie Dilli <ronniedilli@gmail.com>
2020-09-17 20:53:58 -0300

    More refactoring

--- etlelk/run_etl_jobs.py
+++ etlelk/run_etl_jobs.py
@@ -3,8 +3,8 @@ from importlib import import_module
 
 def run_etl_job(config, job_description):
     module = import_module(job_description['module_name'])
-    Cls = getattr(module, job_description['class_name'])
-    o = Cls(config, job_description)
+    class_name = getattr(module, job_description['class_name'])
+    o = class_name(config, job_description)
     o.run(config.es, config.ES_URL)
 
 
@@ -12,5 +12,3 @@ def run_all_etls(config):
     for i in config.INDEXES:
         print(i)
         run_etl_job(config, i)
-
-
